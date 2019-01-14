6976e82a86187ff6b4f468a5aa288ac9467907d5
Said RAKHAOUI <srakhaoui@hotmail.com>
2019-01-14 22:40:13 +0100

    Search skills using ng-select component

--- src/main/webapp/app/entities/cooptation/cooptation-update.component.ts
+++ src/main/webapp/app/entities/cooptation/cooptation-update.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy  } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
-import { catchError, debounceTime, distinctUntilChanged, map, tap, switchMap } from 'rxjs/operators';
+import { Subject, Observable, of, concat } from 'rxjs';
+import {catchError, debounceTime, distinctUntilChanged, map, tap, switchMap} from 'rxjs/operators';
 import * as moment from 'moment';
 import { DATE_TIME_FORMAT } from 'app/shared/constants/input.constants';
 import { JhiAlertService } from 'ng-jhipster';
@@ -12,10 +12,12 @@ import { CooptationService } from './cooptation.service';
 import { ICoopted } from 'app/shared/model/coopted.model';
 import { CooptedService } from 'app/entities/coopted';
 import { ICoopter } from 'app/shared/model/coopter.model';
+import { CoopterService } from 'app/entities/coopter';
 import { ISkill } from 'app/shared/model/skill.model';
 import { SkillService } from 'app/entities/skill';
 
 @Component({
+    changeDetection: ChangeDetectionStrategy.Default,
     selector: 'jhi-cooptation-update',
     templateUrl: './cooptation-update.component.html',
     styles: [`.form-control { width: 300px; display: inline; }`]
@@ -26,20 +28,22 @@ export class CooptationUpdateComponent implements OnInit {
 
     coopteds: ICoopted[];
 
+    coopters: ICoopter[];
+
     skills: ISkill[];
 
-    skill: string;
-
-    searching = false;
-
-    searchFailed = false;
-
     performedOn: string;
-
+    
+    skills$: Observable<string[]>;
+    skillsLoading = false;
+    skillsInput$ = new Subject<string>();
+    selectedSkills: any = [];
+    
     constructor(
         private jhiAlertService: JhiAlertService,
         private cooptationService: CooptationService,
         private cooptedService: CooptedService,
+        private coopterService: CoopterService,
         private skillService: SkillService,
         private activatedRoute: ActivatedRoute
     ) {}
@@ -50,9 +54,28 @@ export class CooptationUpdateComponent implements OnInit {
             this.cooptation = cooptation;
             this.performedOn = this.cooptation.performedOn != null ? this.cooptation.performedOn.format(DATE_TIME_FORMAT) : null;
         });
-        this.cooptedService
-            .query({ filter: 'cooptation-is-null' })
-            .subscribe((res: HttpResponse<ICoopted[]>) => {}, (res: HttpErrorResponse) => this.onError(res.message));
+        this.cooptedService.query({ filter: 'cooptation-is-null' }).subscribe(
+            (res: HttpResponse<ICoopted[]>) => {
+                if (!this.cooptation.coopted || !this.cooptation.coopted.id) {
+                    this.coopteds = res.body;
+                } else {
+                    this.cooptedService.find(this.cooptation.coopted.id).subscribe(
+                        (subRes: HttpResponse<ICoopted>) => {
+                            this.coopteds = [subRes.body].concat(res.body);
+                        },
+                        (subRes: HttpErrorResponse) => this.onError(subRes.message)
+                    );
+                }
+            },
+            (res: HttpErrorResponse) => this.onError(res.message)
+        );
+        this.coopterService.query().subscribe(
+            (res: HttpResponse<ICoopter[]>) => {
+                this.coopters = res.body;
+            },
+            (res: HttpErrorResponse) => this.onError(res.message)
+        );
+        this.loadSkills();
     }
 
     previousState() {
