e02daf28fba9e7eb2ff50696ec3163be7532dc1b
Said RAKHAOUI <srakhaoui@hotmail.com>
2019-02-13 00:04:12 +0100

    Create and save cooptation

--- src/main/java/com/harington/cooptit/service/CooptationService.java
+++ src/main/java/com/harington/cooptit/service/CooptationService.java
@@ -19,6 +19,8 @@ public interface CooptationService {
      * @return the persisted entity
      */
     Cooptation save(Cooptation cooptation);
+    
+    void setUserService(UserService userService);
 
     /**
      * Get all the cooptations.
